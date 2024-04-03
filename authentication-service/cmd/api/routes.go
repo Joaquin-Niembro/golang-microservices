fe88ec5a6a42cf66942a89e197b0e88af9b6ea71
Joaquin Niembro Bueno <v-jobueno@expediagroup.com>
2024-04-03 15:32:06 -0600

    fix issue

--- authentication-service/cmd/api/routes.go
+++ authentication-service/cmd/api/routes.go
@@ -8,22 +8,24 @@ import (
 	"github.com/go-chi/cors"
 )
 
-
 func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	// specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"https://*", "http://*"},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders: []string{"Link"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
-		MaxAge: 300,
+		MaxAge:           300,
 	}))
 
 	mux.Use(middleware.Heartbeat("/ping"))
 
 	mux.Post("/authenticate", app.Authenticate)
+	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("cool bro"))
+	})
 	return mux
 }
